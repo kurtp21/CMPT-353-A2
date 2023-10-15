b772ffc6038ea91e1bb0e9933ada351ca51a7dbe
kurtpagal <zja641@usask.ca>
2023-10-15 16:51:31 -0600

    Completed part B, removed form from html, it was bugging with my prog.

--- server.js
+++ server.js
@@ -14,45 +14,38 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/postmessage', (req, res) => {
     var timestamp = new Date();
-
-    var topic = "<h3>Topic: " + req.body.topic + "</h3>";
-    var body = "<p style=\"font-size: 10px font-weight: 100\">" + req.body.data + "</p>";
-    var time = "<em>Timestamp: " + timestamp.getTime() + "</em>";
-
-    var data = topic + "\n" + body + "\n" + time; 
-
+    
+    var topic = `<h3>Topic: ${req.body.topic}</h3>`;
+    var body = `<p>${req.body.data}</p>`;
+    var time = `<em>[${timestamp.getDate()}/${timestamp.getMonth()}/${timestamp.getFullYear()} @ ${timestamp.getHours()}:${timestamp.getMinutes()}]</em>`;
+    
+    // var data = `<p style=\"font-size: 20px; font-weight: 50;\">${topic}: ${body} ${time}</p>` + '\n';
+    var data = `${topic} ${body} ${time}` + '\n';
     // Write information into the file
     fs.writeFile('pages/posts.txt', data, { flag: 'a+' }, err => {
         if (err) {
             console.error(err);
             res.status(500).send("Failed to write data into file");
-        } else {
-            console.log("Successful writing to file");
-            res.send(JSON.stringify(data));
-        }
+            return
+        } 
+        res.send("Data written successfully");
     });
 });
 
+
 app.get('/', (req, res) => {
     const filePath = path.join(__dirname, "pages", "posts.txt");
-
+    
     fs.readFile(filePath, 'utf8', (err, data) => {
         if (err) {
             console.error(err); 
             res.status(500).send("Failed to read posts.txt");
         } else {
             console.log("Successful reading file data");
-            res.send(JSON.stringify(data));
+            res.send(data);
         }
     });
 });
-
-// app.post('/sayHello', (req,res) => {
-//     var name = req.body.name;
-//     var response = new Object();
-//     response.answer = "hello " + name;         
-//     res.send(JSON.stringify(response));
-// });
 
 app.use('/', express.static('pages'));
 app.listen(PORT, HOST);
